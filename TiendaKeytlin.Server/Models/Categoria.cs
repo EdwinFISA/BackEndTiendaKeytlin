364e92b2a74fa477b38c837e447a0fbdb9e83fb8
EdwinFISA <git config --global user.email>
2025-05-10 01:56:18 -0600

    Estado categoria

--- TiendaKeytlin.Server/Models/Categoria.cs
+++ TiendaKeytlin.Server/Models/Categoria.cs
@@ -1,13 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using TiendaKeytlin.Server.Models;
-
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace TiendaKeytlin.Server.Models
 {
-
-
-
     public class Categoria
     {
         public Categoria()
@@ -23,11 +21,15 @@ namespace TiendaKeytlin.Server.Models
 
         public string Descripcion { get; set; }
 
-        // Relación con EstadoUsuario
+        [Required]
         public int EstadoUsuarioId { get; set; }
 
+        [JsonIgnore]
+        [ValidateNever] // <- clave para evitar validación automática del objeto anidado
+        [ForeignKey("EstadoUsuarioId")]
         public virtual EstadoUsuario EstadoUsuario { get; set; }
-        public virtual ICollection<Productos> Productos { get; set; }
 
+        [JsonIgnore]
+        public virtual ICollection<Productos> Productos { get; set; }
     }
 }
--- TiendaKeytlin.Server/Models/Productos.cs
+++ TiendaKeytlin.Server/Models/Productos.cs
@@ -1,52 +1,47 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using TiendaKeytlin.Server.Models;
 
-namespace TiendaKeytlin.Server.Models
+public class Productos
 {
-    public class Productos
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        [Required]
-        [StringLength(100)]
-        public string Nombre { get; set; }
+    [Required]
+    [StringLength(100)]
+    public string Nombre { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        public string Codigo { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string Codigo { get; set; }
 
-        [StringLength(50)]
-        public string Marca { get; set; }
+    [StringLength(50)]
+    public string Marca { get; set; }
 
-        [Range(0, 999999)]
-        public decimal PrecioAdquisicion { get; set; }
+    [Range(0, 999999)]
+    public decimal PrecioAdquisicion { get; set; }
 
-        [Range(0, 999999)]
-        public decimal PrecioVenta { get; set; }
+    [Range(0, 999999)]
+    public decimal PrecioVenta { get; set; }
 
-        [Range(0, int.MaxValue)]
-        public int Stock { get; set; }
+    [Range(0, int.MaxValue)]
+    public int Stock { get; set; }
 
-        [StringLength(500)]
-        public string? Descripcion { get; set; }
+    [StringLength(500)]
+    public string? Descripcion { get; set; }
 
-        public string? Imagen { get; set; }
+    public string? Imagen { get; set; }
 
-        [Required]
-        public int CategoriaId { get; set; }
+    // Hacer que los campos CategoriaId y ProveedorId sean nullable
+    public int? CategoriaId { get; set; }
 
-        [Required]
-        public int ProveedorId { get; set; }
+    public int? ProveedorId { get; set; }
 
-        [JsonIgnore]
-        [ForeignKey("CategoriaId")]
-        public virtual Categoria Categoria { get; set; }
+    [JsonIgnore]
+    [ForeignKey("CategoriaId")]
+    public virtual Categoria? Categoria { get; set; }
 
-        [JsonIgnore]
-        [ForeignKey("ProveedorId")]
-        public virtual Proveedor Proveedor { get; set; }
-
-    }
+    [JsonIgnore]
+    [ForeignKey("ProveedorId")]
+    public virtual Proveedor? Proveedor { get; set; }
 }
